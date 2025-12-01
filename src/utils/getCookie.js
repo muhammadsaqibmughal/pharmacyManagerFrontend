ec5f0b29bd0b1651b665ff48844c22d3ec4833be
muhammadsaqibmughal <mrsoftware02@gmail.com>
2025-12-01 22:08:47 +0500

    updated

--- src/utils/getCookie.js
+++ src/utils/getCookie.js
@@ -1,8 +1,14 @@
-export const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  debugger;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
-  return null;
-};
+// export const getCookie = (name) => {
+//   const value = `; ${document.cookie}`;
+//   debugger;
+//   const parts = value.split(`; ${name}=`);
+//   if (parts.length === 2) return parts.pop().split(";").shift();
+//   return null;
+// };
 
+
+import Cookies from "js-cookie";
+
+export const getCookie = (name) => {
+ return Cookies.get(name) || null;
+};
