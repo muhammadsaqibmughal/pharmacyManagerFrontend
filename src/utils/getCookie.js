0f4dfdda973b147a280f82398b465626f2c79156
muhammadsaqibmughal <mrsoftware02@gmail.com>
2025-12-01 21:33:17 +0500

    updated code

--- src/utils/getCookie.js
+++ src/utils/getCookie.js
@@ -1,6 +1,6 @@
-
 export const getCookie = (name) => {
   const value = `; ${document.cookie}`;
+  debugger;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop().split(";").shift();
   return null;
